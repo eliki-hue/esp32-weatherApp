df3f43d5e069500679df1c8b2d36629d11707219
Elijah Kiragu <95278300+eliki-hue@users.noreply.github.com>
2024-03-20 12:18:12 +0300

    added collectstatics

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -92,7 +92,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')Run the command: py manage.py collectstatic to collect static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
